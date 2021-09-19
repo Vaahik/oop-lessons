2e047621e559d05729c944ba13d068f6a5e066ba
Vahe Petrosyan <petrosyanvahe@cdigital.am>
2021-09-19 23:02:23 +0400

    Update

---
+++ Polymorphism/oop/Calculate.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace oop
+{
+    class Calculate
+    {
+        public void AddNumbers(int a, int b)
+        {
+            Console.Write("a + b = {0}", a + b);
+        }
+        public void AddNumbers(int a, int b, int c)
+        {
+            Console.WriteLine("a + b + c = {0}", a + b + c);
+        }
+    }
+}
