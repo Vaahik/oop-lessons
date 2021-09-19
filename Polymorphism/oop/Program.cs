425acec099c4d6cb159a58ebc40d966d5f6ff941
Vahe Petrosyan <petrosyanvahe@cdigital.am>
2021-09-20 02:40:18 +0400

    update

--- Polymorphism/oop/Program.cs
+++ Polymorphism/oop/Program.cs
@@ -10,7 +10,11 @@ namespace Polymorphism
     {
         static void Main(string[] args)
         {
-
+            Calculate c = new Calculate();
+            c.AddNumbers(1, 3);
+            c.AddNumbers(1, 4, 7);
+            Console.WriteLine("\nPress Enter Key to Exit..");
+            Console.ReadLine();
         }
     }
 }
---
+++ Polymorphism/oop/Users.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Polymorphism
+{
+    public class Users
+    {
+        public virtual void GetInfo()
+        {
+            Console.WriteLine("Base Class");
+        }
+    }
+    public class Details: Users
+    {
+        public override void GetInfo()
+        {
+            Console.WriteLine("Derived Class");
+        }
+
+    }
+}
